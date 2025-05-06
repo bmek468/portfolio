f4a883a0a25018354fefe29f1569660e2936cb70
keerthanachatlaparthy <chatlaparthykeerthana@gmail.com>
2025-05-05 23:28:36 -0500

    initial commit

---
+++ src/config.js
@@ -0,0 +1,13 @@
+export const config = {
+    username: "bmeka468",
+    name: "Bhanu",
+    headline: "I'm a Full stack web developer having knowledge of Python, Flask, React.JS, TailwindCSS, SQL, PostgreSQL, Supabase, PHP.",
+    location: "Montgomery, Alabama , USA",
+    resumeURL: "https://drive.google.com/file/d/13HHs4Yn9QbIWXfI3rr3BqSrfpBbFfz5R/view?usp=sharing",
+    connectLinks: {
+        github: "https://github.com/bmek468",
+        linkedin: "https://in.linkedin.com/in/bmeka468",
+        twitter: "https://twitter.com/BhanuMeka44539"
+    },
+    formspreeFormURL: import.meta.env.VITE_FORMSPREE_URL
+}
